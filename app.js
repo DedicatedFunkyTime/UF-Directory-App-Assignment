2c34c2e04a91b2a2ced57bc033619c347d741f9f
Ariel <arif93@ufl.edu>
2016-01-14 13:47:02 -0500

    embedded google map looks up the address

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ angular.module('listings', []);
 
 /* register the application and inject all the necessary dependencies */
 var app = angular.module('directoryApp', ['listings']);
+
+app.filter('trusted', ['$sce', function ($sce) {
+	return function(url) {
+		return $sce.trustAsResourceUrl(url);
+	};
+}]);
