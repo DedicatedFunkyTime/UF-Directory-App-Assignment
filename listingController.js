99fa8c8c24770d97d9ece5505fb4892fe26ab08c
Ariel <arif93@ufl.edu>
2016-01-14 20:21:30 -0500

    fixed some basic map functionality and improved the styling of the website

--- listingController.js
+++ listingController.js
@@ -1,10 +1,11 @@
 angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
   function($scope, Listings) {
     $scope.listings = Listings;
-    $scope.currentAddress = undefined;
-    $scope.iframeURL= undefined;
+    $scope.currentAddress = "Please Select Building";
     $scope.newListing = {"name":"","code":"","address":""};
     $scope.sortType = 'code';
+	$scope.iframeURL = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAAvTy5D3B_nzoS1ezbnkg0wG7cCBXM5ms&q=University+of+Florida";
+
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
@@ -21,11 +22,18 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     };
     $scope.showDetails = function(listing) {
     	//var listing = $scope.listings[$scope.listings.indexOf(listing)];
-    	var baseURL = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAAvTy5D3B_nzoS1ezbnkg0wG7cCBXM5ms&q=";
-    	var address = listing["address"];
-    	$scope.currentAddress = address;
-    	address = address.replace(/\s/g,'+');
-    	$scope.iframeURL = baseURL.concat(address);
+		if("address" in listing){
+			var address = listing["address"];
+			var baseURL = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAAvTy5D3B_nzoS1ezbnkg0wG7cCBXM5ms&q=";
+			$scope.currentAddress = address;
+			address = address.replace(/\s/g,'+');
+			$scope.iframeURL = baseURL.concat(address);
+			console.log("address exists!");
+		}else{
+			$scope.currentAddress = "No address available";
+			$scope.iframeURL = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAAvTy5D3B_nzoS1ezbnkg0wG7cCBXM5ms&q=Gainesvile,+FL";
+			console.log("address does not exist!");
+		}
     };
   }
 ]);
